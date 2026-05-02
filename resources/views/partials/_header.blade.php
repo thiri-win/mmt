1adb7b8a55e780a823cd16c720302b0bf187594e
Thiri Win <thiriwinzhuang@gmail.com>
2026-05-02 07:22:00 +0630

    removeimport and add link

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -5,8 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Padauk:wght@400;700&display=swap" rel="stylesheet">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Padauk:wght@400;700&display=swap');
+        /* @import url('https://fonts.googleapis.com/css2?family=Padauk:wght@400;700&display=swap'); */
 
         /* @font-face {
             font-family: 'padauk';
