7f034c8c0946430984bdb0ac4a684df2fed9c23c
Thiri Win <thiriwinzhuang@gmail.com>
2026-05-02 10:07:17 +0630

    adding file:// to fontface src

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -9,7 +9,8 @@
 
         @font-face {
             font-family: 'padauk';
-            src: url('{{ storage_path('fonts/Padauk-Regular.ttf') }}') format('truetype');
+            /* src: url('{{ storage_path('fonts/Padauk-Regular.ttf') }}') format('truetype'); */
+            src: url("file://{{ storage_path('fonts/Padauk-Regular.ttf') }}") format("truetype");
             font-weight: normal;
             font-style: normal;
         }
