9b008e70a25d790a28086242f9dc7d3e5d4b9589
Thiri Win <thiriwinzhuang@gmail.com>
2026-05-02 06:45:06 +0630

    add full html tag to _header.blade.php

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,49 +1,60 @@
-<style>
-    @font-face {
-        font-family: 'padauk';
-        src: url('{{ storage_path("fonts/Padauk-Regular.ttf") }}') format('truetype');
-        font-weight: normal;
-        font-style: normal;
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: 'padauk' !important;
-        font-size: 14px;
-        line-height: 1.7;
-        text-rendering: optimizeLegibility;
-    }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        @font-face {
+            font-family: 'padauk';
+            src: url('{{ storage_path('fonts/Padauk-Regular.ttf') }}') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
 
-    header {
-        width: 100%;
-        /* padding-top: 10mm; */
-    }
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            font-family: 'padauk' !important;
+            font-size: 14px;
+            line-height: 1.7;
+        }
 
-    .heading {
-        text-align: center;
-        margin-top: 10mm;
-        margin-bottom: 5mm;
-    }
+        header {
+            width: 100%;
+            /* padding-top: 10mm; */
+        }
 
-    .heading h1 {
-        font-size: 18px;
-    }
+        .heading {
+            text-align: center;
+            margin-top: 10mm;
+            margin-bottom: 5mm;
+        }
 
-    .heading h2 {
-        font-size: 16px;
-    }
+        .heading h1 {
+            font-size: 18px;
+        }
 
-    .heading p {
-        font-size: 15px;
-    }
-</style>
+        .heading h2 {
+            font-size: 16px;
+        }
 
-<header>
-    <div class="heading">
-        <h1>ကိုမောင်မောင်သန်း နှင့် ညီအကိုများ</h1>
-        <h2>ဘိလပ်မြေ၊ အုတ်၊ သဲ၊ ကျောက် ရောင်းဝယ် သယ်ယူပို့ဆောင်ရေး</h2>
-        <p>&phone; 09-443060657, 09-788464997, 09-5167659, 09-765167659</p>
-    </div>
-</header>
+        .heading p {
+            font-size: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <div class="heading">
+            <h1>ကိုမောင်မောင်သန်း နှင့် ညီအကိုများ</h1>
+            <h2>ဘိလပ်မြေ၊ အုတ်၊ သဲ၊ ကျောက် ရောင်းဝယ် သယ်ယူပို့ဆောင်ရေး</h2>
+            <p>&phone; 09-443060657, 09-788464997, 09-5167659, 09-765167659</p>
+        </div>
+    </header>
+</body>
+
+</html>
