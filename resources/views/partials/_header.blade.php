d184f9bca74abb62e4f03a484f61bc9bb8b39332
Thiri Win <thiriwinzhuang@gmail.com>
2026-05-02 06:35:33 +0630

    font family change to paduak

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -10,7 +10,7 @@
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: poppins;
+        font-family: 'padauk';
         font-size: 14px;
         line-height: 1.7;
     }
